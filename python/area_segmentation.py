5608180e00a4cad3c5ae8fd100bbceef7cd3f210
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-11-21 18:30:10 +0100

    Removing debug output

--- python/area_segmentation.py
+++ python/area_segmentation.py
@@ -87,8 +87,6 @@ def get_center_pos(im, pos, size):
                 max_y = max(max_y, y)
     r_x = int((abs(max_x - min_x) / 2) + (b_x * width))
     r_y = int((abs(max_y - min_y) / 2) + (b_y * height))
-    if pos == (7, 4):
-        print(max_x, min_x, max_y, min_y)
     return r_x, r_y
 
 
